fdb4a230ad430cc4a496e7d5222f9ef2df4e3010
trietle847 <trietle847@gmail.com>
2025-06-08 07:01:27 +0700

    add rejectRequest

--- frontend/src/services/friend.service.ts
+++ frontend/src/services/friend.service.ts
@@ -1,14 +1,24 @@
 import createAPI from "./createAPI.service";
 
 const FriendService = {
-    getMyFriend: async() => {
-        const response = await createAPI.get("/friend/myFriend")
-        return response.data;
-    },
-    getRequestAddFriend: async() => {
-        const response = await createAPI.get("/friend/requestAddFriend")
-        return response.data;
-    } 
-}
+  getMyFriend: async () => {
+    const response = await createAPI.get("/friend/myFriend");
+    return response.data;
+  },
+  getRequestAddFriend: async () => {
+    const response = await createAPI.get("/friend/requestAddFriend");
+    return response.data;
+  },
+  acceptRequest: async (idRequest: string) => {
+    const response = await createAPI.put("/friend/accept",{idRequest});
+    return response;
+  },
+  rejectRequest: async (idRequest: string) => {
+    const response = await createAPI.delete("/friend/reject", {
+      data: { idRequest },
+    });
+    return response;
+  }
+};
 
 export default FriendService
