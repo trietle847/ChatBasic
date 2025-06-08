cb6672766ece1e8544887295796073be7c205528
trietle847 <trietle847@gmail.com>
2025-06-08 21:05:33 +0700

    update

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,6 @@ const config = require("./app/config/mongoDB");
 const MongoDB = require("./app/utils/mongodb.util");
 const http = require("http");
 const socketUtil = require("./app/utils/socket.util");
-const messageSchema = require("./app/models/message.model");
 
 async function startServer() {
   try {
@@ -16,35 +15,35 @@ async function startServer() {
 
     const io = socketUtil.init(server);
 
-    io.on("connection", (socket) => {
-      console.log(`user connection ${socket.id}`);
+    // io.on("connection", (socket) => {
+    //   console.log(`user connection ${socket.id}`);
 
-      socket.on("join_room", (room) => {
-        socket.join(room);
-        console.log(`sokect ${socket.id} joined room ${room}`);
-      });
+    //   socket.on("join_room", (room) => {
+    //     socket.join(room);
+    //     console.log(`sokect ${socket.id} joined room ${room}`);
+    //   });
 
-      socket.on("send_message", async (data) => {
-        try {
-          const message = await messageSchema.create({
-            conversationId: data.conversationId,
-            senderId: data.senderId,
-            content: data.content,
-            type: data.type,
-          });
+    //   socket.on("send_message", async (data) => {
+    //     try {
+    //       const message = await messageSchema.create({
+    //         conversationId: data.conversationId,
+    //         senderId: data.senderId,
+    //         content: data.content,
+    //         type: data.type,
+    //       });
 
-          const populateMessage = await message.populate("senderId", "hoten email")
-          io.to(data.conversationId).emit("receive_message", populateMessage);
-        } catch (error) {
-            console.error("Lỗi khi lưu tin nhắn:", error);
-            socket.emit("error_message", { error: "Gửi tin nhắn thất bại." });
-        }
-      });
+    //       const populateMessage = await message.populate("senderId", "hoten email")
+    //       io.to(data.conversationId).emit("receive_message", populateMessage);
+    //     } catch (error) {
+    //         console.error("Lỗi khi lưu tin nhắn:", error);
+    //         socket.emit("error_message", { error: "Gửi tin nhắn thất bại." });
+    //     }
+    //   });
 
-      socket.on("disconnect", () => {
-        console.log(`user disconnected ${socket.id}`);
-      });
-    });
+    //   socket.on("disconnect", () => {
+    //     console.log(`user disconnected ${socket.id}`);
+    //   });
+    // });
 
     server.listen(PORT, () => {
       console.log(`server is running on port ${PORT}`);
