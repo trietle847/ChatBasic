b48b50119c944ce387d36b2972103f0016c0c156
Triết Lê <trietle847@example.com>
2025-06-12 15:38:33 +0700

    use Toast and add Notification permission

--- frontend/src/socket/socketContex.tsx
+++ frontend/src/socket/socketContex.tsx
@@ -1,6 +1,7 @@
 // src/socketContext.tsx
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
+import { toast } from "sonner";
 
 const SOCKET_SERVER_URL = "http://localhost:3000";
 
@@ -15,11 +16,32 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
     const newSocket = io(SOCKET_SERVER_URL);
     setSocket(newSocket);
 
+    newSocket.on("receive_friend_request", () => {
+      if (document.hidden && "Notification" in window && Notification.permission === "granted") {
+        new Notification("Bạn có Thông báo mới", {
+          body: "Ai đó vừa gửi yêu cầu kết bạn!",
+        });
+      }
+      else {
+        toast("Bạn có lời kết bạn mới", {
+          description: "Bạn vừa nhận được một lời kết bạn",
+          duration: 5000,
+        });
+      }
+    }
+  )
+
     // Khi đóng tab hoặc reload, socket sẽ tự disconnect
     return () => {
       newSocket.disconnect();
     };
   }, []);
+
+  useEffect(() => {
+    if ("Notification" in window && Notification.permission !== "granted"){
+      Notification.requestPermission();
+    }
+  }, [])
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
