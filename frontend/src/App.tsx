b6facd0629229e778c8719810ab0fd31620fd394
Triết Lê <trietle847@example.com>
2025-06-15 20:57:55 +0700

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,26 @@ import LoginPage from "@/pages/login";
 import RegisterForm from "./pages/register";
 import HomePage from "./pages/home_simple_chat";
 import ListFriendPage from "./pages/friend/FriendPage";
+import { AgoraProvider } from "@/context/AgoraContext";
 
 import { SocketProvider } from "@/socket/socketContex";
 import { Toaster } from "sonner";
 
 function App() {
   return (
-    <SocketProvider>
-      <Toaster position="top-right" />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/friend" element={<ListFriendPage />} />
-      </Routes>
-    </SocketProvider>
+    <AgoraProvider>
+      <SocketProvider>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/friend" element={<ListFriendPage />} />
+        </Routes>
+      </SocketProvider>
+    </AgoraProvider>
   );
 }
+
 
 export default App;
