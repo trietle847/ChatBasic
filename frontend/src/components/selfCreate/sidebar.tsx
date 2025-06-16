30dce678fb82cf593c0ae52ade2d7e413ec998e0
Triết Lê <trietle847@example.com>
2025-06-16 20:59:33 +0700

    update url avatar

--- frontend/src/components/selfCreate/sidebar.tsx
+++ frontend/src/components/selfCreate/sidebar.tsx
@@ -1,9 +1,25 @@
+import userService from "@/services/user.service";
 import { faMessage, faAddressBook, faGears } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
     const navigate = useNavigate();
+    const [avatarUrl, setAvataUrl] = useState<string|null>(null);
+
+    useEffect(() => {
+      const fetchUser = async () => {
+        try {
+          const me = await userService.getMe();
+          setAvataUrl(me.user.avatar);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      fetchUser();
+    })
   return (
     <div className="w-16 bg-[#0068FF] flex flex-col items-center py-4 space-y-4 text-white">
       <div
@@ -11,7 +27,7 @@ const Sidebar = () => {
         className="w-10 h-10 rounded-full overflow-hidden"
       >
         <img
-          src="https://i.pravatar.cc/150?u=${conv._id}"
+          src={avatarUrl || "https://via.placeholder.com/150"}
           alt="Avatar"
           className="w-full h-full object-cover"
         />
