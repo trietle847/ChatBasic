9911e71f96fafe058b7bafd2ec92499126f73da8
Triết Lê <trietle847@example.com>
2025-06-16 20:12:14 +0700

    add router upload-avatar

--- Backend/app/routes/user.route.js
+++ Backend/app/routes/user.route.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const user = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware")
+const upload = require("../config/upload");
 
 const router = express.Router();
 
 router.route("/")
     .post(user.create)
     .put(authMiddleware,user.update)
+
+router.route("/upload-avatar")
+    .post(authMiddleware,upload.single("avatar"), user.uploadAvatar)   
 
 router.route("/find/:id")
     .get(user.getUserById)
