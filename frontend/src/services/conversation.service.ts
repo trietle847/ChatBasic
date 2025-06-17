f2682c159a745dedb4f71f8ec08a4566836b6474
Triết Lê <trietle847@example.com>
2025-06-17 14:43:24 +0700

    add api create Conversation

--- frontend/src/services/conversation.service.ts
+++ frontend/src/services/conversation.service.ts
@@ -1,9 +1,18 @@
 import createAPI from "./createAPI.service";
 
+export interface GroupData {
+    memberIds: string[];
+    name: string;
+    type: "group";
+}
 const conversationService = {
     getConversation: async() => {
         const response = await createAPI.get("/conversation");
         return response;
+    },
+    createConversation: async(data: GroupData) => {
+        const response = await createAPI.post("/conversation", data);
+        return response.data;
     }
 }
 
