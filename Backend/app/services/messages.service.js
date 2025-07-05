3372a96440fafa241a90f9fc70e2e9b2fb6e76b2
Triết Lê <trietle847@example.com>
2025-07-05 08:53:38 +0700

    count message unread

--- Backend/app/services/messages.service.js
+++ Backend/app/services/messages.service.js
@@ -3,12 +3,7 @@ const mongoose = require("mongoose");
 
 class MessageService {
   async sendMessage(conversationId, senderId, content) {
-    const message = new Message(
-      conversationId,
-      senderId,
-      content,
-    );
-    
+    const message = new Message(conversationId, senderId, content);
 
     return await message.save();
   }
@@ -20,10 +15,33 @@ class MessageService {
   }
 
   async getMessagesById(messageId) {
-    console.log(messageId)
+    console.log(messageId);
     return await Message.findOne({
       _id: new mongoose.Types.ObjectId(messageId),
     });
+  }
+
+  async getUnreadMap(userId) {
+    const result = await Message.aggregate([
+      {
+        $match: {
+          readBy: { $ne: new mongoose.Types.ObjectId(userId) },
+        },
+      },
+      {
+        $group: {
+          _id: "$conversationId",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    const unreadMap = {};
+    result.forEach((item) => {
+      unreadMap[item._id.toString()] = item.count;
+    });
+
+    return unreadMap;
   }
 }
 
