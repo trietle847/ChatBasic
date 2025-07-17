c1993d4defc5a078922ac7f62e0c922c359481f1
Triết Lê <trietle847@example.com>
2025-07-17 14:43:00 +0700

    update

--- frontend/src/socket/socketContex.tsx
+++ frontend/src/socket/socketContex.tsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { toast } from "sonner";
 
-const SOCKET_SERVER_URL = "http://localhost:3000";
+const SOCKET_SERVER_URL = "https://chatbasic-backend.onrender.com"; //"http://localhost:3000";
 
 const SocketContext = createContext<Socket | null>(null);
 
@@ -17,19 +17,21 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
     setSocket(newSocket);
 
     newSocket.on("receive_friend_request", () => {
-      if (document.hidden && "Notification" in window && Notification.permission === "granted") {
+      if (
+        document.hidden &&
+        "Notification" in window &&
+        Notification.permission === "granted"
+      ) {
         new Notification("Bạn có Thông báo mới", {
           body: "Ai đó vừa gửi yêu cầu kết bạn!",
         });
-      }
-      else {
+      } else {
         toast("Bạn có lời kết bạn mới", {
           description: "Bạn vừa nhận được một lời kết bạn",
           duration: 5000,
         });
       }
-    }
-  )
+    });
 
     // Khi đóng tab hoặc reload, socket sẽ tự disconnect
     return () => {
@@ -38,10 +40,10 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   }, []);
 
   useEffect(() => {
-    if ("Notification" in window && Notification.permission !== "granted"){
+    if ("Notification" in window && Notification.permission !== "granted") {
       Notification.requestPermission();
     }
-  }, [])
+  }, []);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
